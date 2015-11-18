d977986d9cd6bef114b16cf7ba44e774300448ed
Philipp <philipp.kropik@gmail.com>
2015-11-18 21:22:32 +0100

    Update

--- src/matrix.java
+++ src/matrix.java
@@ -3,33 +3,48 @@ public class matrix {
 
 	public static void main (String[] args)
 	{
-		int x = 7;
+
 		
-		generateIdentityMatrix(x);
+		double[][] pintable=(generateIdentityMatrix(1,7));
+		printMatrix(pintable);
+System.out.println("");
+		
+		double[][] pintable2=(generateIdentityMatrix(5,5));
+		printMatrix(pintable2);
 	}
 	
-	public static double[][] generateIdentityMatrix(int size)
-	{
-		double [][] a = new double [size][size];
 	
-		for (int i=0; i<size;i++)
+	public static double[][] generateIdentityMatrix(int size1, int size2)
+	{
+		double [][] matrix = new double [size1][size2];
+	 /*Wenn Quadratisch:
+	  * for(int i=0;z<size1;z++)
+	  * matrix[i][i] = 1;
+	  */
+		for (int i=0; i<size1;i++)
 		{
-			for (int n=0;n<size;n++)
+			for (int n=0;n<size2;n++)
 			{
 				if (i ==n)
 				{
-					a[i][n]=1;
-					System.out.print(a[i][n]+" ");
+					matrix[i][n]=1;
 				}
-				else
-				{
-					a[i][n]=0;
-					System.out.print(a[i][n]+" ");
-				}
+			}
+		}
+		return matrix;
+	}
+
+	public static void printMatrix(double[][] matrix)
+	{
+	
+		for (int i=0; i<matrix.length;i++)
+		{
+			for (int n=0;n<matrix[i].length;n++)
+			{
+				System.out.print(matrix[i][n]+"\t");
 			}
 			System.out.println("");
 		}
-		return a;
-	}
 	}
 
+}
